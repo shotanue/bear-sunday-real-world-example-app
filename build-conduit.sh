575a683d4462080bd8a1aeb50407b594cba9b4b3
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-03 06:03:44 +0900

    mod actions

--- build-conduit.sh
+++ build-conduit.sh
@@ -2,4 +2,6 @@
 
 docker image build --target=base-image --tag=multi-stage:base-image Conduit
 docker image build --target=test-package --tag=multi-stage:test-package Conduit
-docker image build --target=ship --tag=multi-stage:ship Conduit
+
+TAG="docker.pkg.github.com/${{ github.repository }}/bear-sunday-real-world-example-app:${SHA}"
+docker image build --target=ship --tag=${TAG} Conduit
