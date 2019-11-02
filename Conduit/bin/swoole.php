4c5be96d6e68ef088ec6794dbc246eff53599af5
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-02 23:26:19 +0900

    format

--- Conduit/bin/swoole.php
+++ Conduit/bin/swoole.php
@@ -1,12 +1,10 @@
 <?php
+declare(strict_types=1);
 require dirname(__DIR__) . '/autoload.php';
-$bootstrap = dirname(__DIR__) . '/vendor/bear/swoole/bootstrap.php';
-if (! file_exists($bootstrap)) {
-    throw new \LogicException('"bear/swoole" is not installed. See http://bearsunday.github.io/manuals/1.0/en/swoole.html');
+
+$bootstrap = require dirname(__DIR__) . '/vendor/bear/swoole/bootstrap.php';
+if (!file_exists($bootstrap)) {
+    throw new LogicException('"bear/swoole" is not installed. See http://bearsunday.github.io/manuals/1.0/en/swoole.html');
 }
-exit((require $bootstrap)(
-    'prod-app',
-    'Acme\Conduit',
-    '0.0.0.0',
-    '8080'
-));
+
+exit(($bootstrap)('prod-app', 'Acme\Conduit', '0.0.0.0', '8080'));
