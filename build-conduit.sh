bf082e97f76c6da77f2bff00c44ad90828177bb8
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-03 16:43:01 +0900

    remove script for ci(unused)

--- build-conduit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker image build --target=base-image --tag=multi-stage:base-image Conduit
-docker image build --target=test-package --tag=multi-stage:test-package Conduit
-
-TAG="docker.pkg.github.com/${{ github.repository }}/bear-sunday-real-world-example-app:${SHA}"
-docker image build --target=ship --tag=${TAG} Conduit
