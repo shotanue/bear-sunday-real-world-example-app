9773e529ade2591768cd8a49bcb22357d06096fe
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-04 23:18:39 +0900

    setup migration

--- Conduit/bin/setup.php
+++ Conduit/bin/setup.php
@@ -1,5 +1,23 @@
 <?php
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/autoload.php';
+require_once dirname(__DIR__) . '/env.php';
+
+// dir
 chdir(dirname(__DIR__));
 passthru('rm -rf var/tmp/*');
 passthru('chmod 775 var/tmp');
 passthru('chmod 775 var/log');
+
+
+// db
+$dsn = 'mysql:host=' . getenv('DB_HOST');
+$userName = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+
+$pdo = new PDO($dsn, $userName, $pass);
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME') . '_test');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e test');
