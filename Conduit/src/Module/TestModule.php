3bbe117e8db9647d3c584b188216430f3a0fe05d
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-09 17:00:46 +0900

    setup module

---
+++ Conduit/src/Module/TestModule.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Conduit\Module;
+
+use BEAR\Package\AbstractAppModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+
+class TestModule extends AbstractAppModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->install(
+            new AuraSqlModule(
+                getenv('DB_DSN') . '_test',
+                getenv('DB_USER'),
+                getenv('DB_PASS'),
+                getenv('DB_SLAVE')
+            )
+        );
+    }
+}
