ca4bac3a9cd11062c79e44a9dc2d92a9f8ae4dba
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-11 00:37:33 +0900

    comment out db-setup script

--- Conduit/bin/setup.php
+++ Conduit/bin/setup.php
@@ -12,10 +12,10 @@ passthru('chmod 775 var/log');
 
 
 // db
-$dsn = 'mysql:host=' . getenv('DB_HOST');
-$userName = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+//$dsn = 'mysql:host=' . getenv('DB_HOST');
+//$userName = getenv('DB_USER');
+//$pass = getenv('DB_PASS');
 
-$pdo = new PDO($dsn, $userName, $pass);
-$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME'));
-$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME') . '_test');
+//$pdo = new PDO($dsn, $userName, $pass);
+//$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME'));
+//$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME') . '_test');
