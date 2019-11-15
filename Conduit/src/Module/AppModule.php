5674cfd8345dcce0b793951a4e4e65fb5c3266a9
shotanue <22065594+shotanue@users.noreply.github.com>
2019-11-16 02:31:00 +0900

    set pdo options

--- Conduit/src/Module/AppModule.php
+++ Conduit/src/Module/AppModule.php
@@ -8,6 +8,7 @@ use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
 use BEAR\Resource\Module\JsonSchemaModule;
+use PDO;
 use Ray\AuraSqlModule\AuraSqlModule;
 use Ray\IdentityValueModule\IdentityValueModule;
 use Ray\Query\SqlQueryModule;
@@ -23,12 +24,18 @@ class AppModule extends AbstractAppModule
         $appDir = $this->appMeta->appDir;
         /** @noinspection PhpIncludeInspection */
         require_once $appDir . '/env.php';
+        $pdoOptions = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
         $this->install(
             new AuraSqlModule(
                 getenv('DB_DSN'),
                 getenv('DB_USER'),
                 getenv('DB_PASS'),
-                getenv('DB_SLAVE')
+                getenv('DB_SLAVE'),
+                $pdoOptions
             )
         );
         $this->install(new SqlQueryModule($appDir . '/var/sql'));
