026f87a356dcccff50e2c0feb5bbf474c1bfd08f
shotanue <22065594+shotanue@users.noreply.github.com>
2019-12-08 21:40:42 +0900

    PHP version downgrade, because of 7.4 unit test failure.

--- Conduit/bin/migrate_db.php
+++ Conduit/bin/migrate_db.php
@@ -9,17 +9,23 @@ $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASS');
 $dbName = sprintf($argv[1] === 'test' ? '%s_test' : '%s', getenv('DB_NAME'));
 
-$createPdo = fn() => new PDO(sprintf('mysql:host=%s', $dbHost), $dbUser, $dbPass);
-$migrate = fn($resourceName) => passthru(
-    "mysqldef -u=${dbUser} -p=${dbPass} -h=${dbHost} ${dbName}_${resourceName} < var/schema/${resourceName}.schema.sql"
-);
+$createPdo = function () use ($dbHost, $dbUser, $dbPass) {
+    return new PDO(sprintf('mysql:host=%s', $dbHost), $dbUser, $dbPass);
+};
+$migrate = function ($resourceName) use ($dbUser, $dbPass, $dbHost, $dbName) {
+    $command = "mysqldef -u=${dbUser} -p=${dbPass} -h=${dbHost} ${dbName}_${resourceName} < var/schema/${resourceName}.schema.sql";
+    passthru($command);
+};
 
 $createSchema = function (string $resourceName) use ($dbName, $createPdo, $migrate) {
     ($createPdo)()->exec("CREATE DATABASE IF NOT EXISTS {$dbName}_${resourceName}");
     ($migrate)($resourceName);
 };
 
-array_map(fn(string $resourceName) => ($createSchema)($resourceName), [
+$callback = function (string $resourceName) use ($createSchema) {
+    ($createSchema)($resourceName);
+};
+array_map($callback, [
     'user',
     'article',
 ]);
