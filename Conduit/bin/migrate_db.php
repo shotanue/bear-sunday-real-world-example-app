10a0015f6f994fc25290779058556e7fbafb5127
shotanue <22065594+shotanue@users.noreply.github.com>
2020-02-03 02:50:53 +0900

    simplify migrate

--- Conduit/bin/migrate_db.php
+++ Conduit/bin/migrate_db.php
@@ -9,23 +9,7 @@ $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASS');
 $dbName = sprintf($argv[1] === 'test' ? '%s_test' : '%s', getenv('DB_NAME'));
 
-$createPdo = function () use ($dbHost, $dbUser, $dbPass) {
-    return new PDO(sprintf('mysql:host=%s', $dbHost), $dbUser, $dbPass);
-};
-$migrate = function ($resourceName) use ($dbUser, $dbPass, $dbHost, $dbName) {
-    $command = "mysqldef -u=${dbUser} -p=${dbPass} -h=${dbHost} ${dbName}_${resourceName} < var/schema/${resourceName}.schema.sql";
-    passthru($command);
-};
-
-$createSchema = function (string $resourceName) use ($dbName, $createPdo, $migrate) {
-    ($createPdo)()->exec("CREATE DATABASE IF NOT EXISTS {$dbName}_${resourceName}");
-    ($migrate)($resourceName);
-};
-
-$callback = function (string $resourceName) use ($createSchema) {
-    ($createSchema)($resourceName);
-};
-array_map($callback, [
-    'user',
-    'article',
-]);
+$pdo = new PDO(sprintf('mysql:host=%s', $dbHost), $dbUser, $dbPass);
+$pdo->exec("CREATE DATABASE IF NOT EXISTS {$dbName}");
+passthru('cat var/schema/*.schema.sql > /tmp/tmp.schema.sql');
+passthru("mysqldef -u=${dbUser} -p=${dbPass} -h=${dbHost} ${dbName} < /tmp/tmp.schema.sql");
